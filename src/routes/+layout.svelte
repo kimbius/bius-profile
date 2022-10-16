3d238e167f9d340f58387074f34e5e18ae5c7148
Kimbius <kimbius.no1@gmail.com>
2022-10-17 01:43:08 +0700

    update

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+    import "../app.css";
+</script>
+
+<div class="min-h-screen">
+    <slot />
+</div>
+
+<footer class="bg-green-400">
+    <div class="container py-5 font-bold text-center">
+        <h1 class="text-white text-sm font-medium">ช่องทางการติดต่อ</h1>
+        <div class="flex gap-x-4 flex-wrap text-base justify-center text-white">
+            <a href="//fb.com/bius.peerawit">Facebook</a>
+            <a href="//github.com/kimbius">Github</a>
+            <a href="//discord.com">Kimbius#0274</a>
+            <a href="mailto:kimbius.no1@gmail.com">Mail</a>
+        </div>
+    </div>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,227 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div
+    class="min-h-[50vh] flex items-center bg-gradient-to-t from-green-400 to-green-300"
+>
+    <div class="container flex justify-center text-center">
+        <div class="font-bold">
+            <p class="text-xl text-green-900 leading-none">สวัสดี! นี่คือ</p>
+            <h1 class="text-6xl my-2 text-green-100">" KIMBIUS "</h1>
+            <div class="flex justify-center gap-2">
+                <div
+                    class="px-3 py-1 rounded-full bg-green-400 border border-green-300 text-green-100 text-xs md:text-sm"
+                >
+                    FULLSTACK DEVELOPER
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container py-5 pb-14">
+    <div>
+        <div class="text-center mt-5 mb-10">
+            <h1 class="text-2xl font-bold text-green-400">
+                ผลงานและประสบการณ์
+            </h1>
+            <p class="font-bold text-gray-600">WORKS AND EXPERIENCE</p>
+        </div>
+        <div class="grid md:grid-cols-2 gap-4">
+            <div>
+                <div>
+                    <h1 class="text-sm font-bold text-gray-800">ผลงาน</h1>
+                </div>
+                <div class="mt-2">
+                    <ul class="flex flex-col gap-4">
+                        <li class="md:pl-2">
+                            <h2 class="text-xs leading-none">
+                                เข้าร่วมการแข่งขัน THAILAND CYBER TOP TALENT
+                                <span class="text-xs text-green-400"
+                                    >( 2021 )</span
+                                >
+                            </h2>
+                            <div class="mt-2">
+                                <img
+                                    width="350"
+                                    src="https://media.discordapp.net/attachments/880832793431670825/910376030802440222/Participation_PEERAWIT_PIWSAWANG-01.jpg?width=1470&height=1039"
+                                    alt=""
+                                />
+                            </div>
+                        </li>
+                        <li class="md:pl-2">
+                            <h2 class="text-xs leading-none">
+                                เข้าร่วมอบรมของ Inspire IT65 by KMITL
+                                <span class="text-xs text-green-400"
+                                    >( 2021 )</span
+                                >
+                            </h2>
+                            <div class="mt-2">
+                                <img
+                                    width="350"
+                                    src="https://cdn.discordapp.com/attachments/880832793431670825/917332567622029322/Interactiveport_certificate69-1.png?width=1470&height=103"
+                                    alt=""
+                                />
+                            </div>
+                        </li>
+
+                        <li class="md:pl-2">
+                            <h2 class="text-xs leading-none">
+                                สร้างเว็บไซต์ BloxcodesTH ภายใต้ MultilabX
+                                <span class="text-xs text-green-400"
+                                    >( 2022 )</span
+                                >
+                            </h2>
+                            <div class="mt-2">
+                                <img
+                                    width="350"
+                                    src="https://media.discordapp.net/attachments/938043441936490526/1030809728240058469/2565-10-15_18.49.42.png?width=2352&height=1403"
+                                    alt=""
+                                />
+                            </div>
+                        </li>
+
+                        <li class="md:pl-2">
+                            <h2 class="text-xs leading-none">
+                                สร้าง Module สำหรับ generate qrcode
+                                โอนเงินของแอพ Truemoney Wallet
+                                <span class="text-xs text-green-400"
+                                    >( 2022 )</span
+                                >
+                            </h2>
+                            <div class="mt-2">
+                                <a
+                                    href="https://github.com/kimbius/Truewallet-Qr-Generator"
+                                    target="_"
+                                >
+                                    <img
+                                        src="https://img.shields.io/badge/GitHub-source%20code-lightgrey"
+                                        alt=""
+                                    />
+                                </a>
+                            </div>
+                        </li>
+
+                        <li class="md:pl-2">
+                            <h2 class="text-xs leading-none">
+                                สร้างเว็บไซต์ JamemyShop ภายใต้ MultilabX
+                                <span class="text-xs text-green-400"
+                                    >( 2022 )</span
+                                >
+                            </h2>
+                            <div class="mt-2">
+                                <img
+                                    width="350"
+                                    src="https://media.discordapp.net/attachments/879011747309191229/1031270702600425583/unknown.png?width=1830&height=1041"
+                                    alt=""
+                                />
+                            </div>
+                        </li>
+
+                        <li class="md:pl-2">
+                            <h2 class="text-xs leading-none">
+                                สร้างเว็บไซต์ Toppy Termgame
+                                <span class="text-xs text-green-400"
+                                    >( 2022 )</span
+                                >
+                            </h2>
+                            <div class="mt-2">
+                                <img
+                                    width="350"
+                                    src="https://media.discordapp.net/attachments/879011747309191229/1031273969887744080/unknown.png?width=1830&height=1041"
+                                    alt=""
+                                />
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="flex flex-col gap-4">
+                <div>
+                    <div>
+                        <h1 class="text-sm font-bold text-gray-800">
+                            ประสบการณ์
+                        </h1>
+                    </div>
+                    <div class="mt-2">
+                        <div class="flex flex-wrap gap-2">
+                            <div
+                                class="px-3 py-1 rounded-full bg-yellow-400 text-white text-sm"
+                            >
+                                javascript
+                            </div>
+                            <div
+                                class="px-3 py-1 rounded-full bg-blue-500 text-white text-sm"
+                            >
+                                typescript
+                            </div>
+                            <div
+                                class="px-3 py-1 rounded-full bg-green-500 text-white text-sm"
+                            >
+                                node
+                            </div>
+                            <div
+                                class="px-3 py-1 rounded-full bg-green-600 text-white text-sm"
+                            >
+                                nuxt2
+                            </div>
+                            <div
+                                class="px-3 py-1 rounded-full bg-orange-600 text-white text-sm"
+                            >
+                                svelte
+                            </div>
+                            <div
+                                class="px-3 py-1 rounded-full bg-red-500 text-white text-sm"
+                            >
+                                nest
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <h1 class="text-sm font-bold text-gray-800">
+                            สถานที่ทำงาน
+                        </h1>
+                    </div>
+                    <div class="mt-2">
+                        <ul class="flex flex-col gap-3">
+                            <li class="pl-2">
+                                <h2 class="text-lg leading-none font-bold">
+                                    SEEYOU STUDIO
+                                    <span class="text-xs text-green-400"
+                                        >( 2020 - 2021 )</span
+                                    >
+                                </h2>
+                                <p class="text-sm">
+                                    <span class="font-medium">ตำแหน่งงาน</span> Backend
+                                    Developer, Game Optimizer
+                                </p>
+                            </li>
+                            <li class="pl-2">
+                                <h2 class="text-lg leading-none font-bold">
+                                    SRIWISA GROUP
+                                    <span class="text-xs text-green-400"
+                                        >( 2021 - ปัจจุบัน )</span
+                                    >
+                                </h2>
+                                <p class="text-sm">
+                                    <span class="font-medium">ตำแหน่งงาน</span> Board
+                                    of Director, Programmer
+                                </p>
+                            </li>
+                            <li class="pl-2">
+                                <h2 class="text-lg leading-none font-bold">
+                                    MULTILABX
+                                    <span class="text-xs text-green-400"
+                                        >( 2021 - ปัจจุบัน )</span
+                                    >
+                                </h2>
+                                <p class="text-sm">
+                                    <span class="font-medium">ตำแหน่งงาน</span> Frontend
+                                    Developer
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
